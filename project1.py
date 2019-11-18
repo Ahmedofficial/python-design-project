ac3d477abbd6fe025cf8be935dac8f0db3a4ca78
Ahmedofficial <33352464+Ahmedofficial@users.noreply.github.com>
2019-11-18 14:40:10 -0500

    Add files via upload

---
+++ project1.py
@@ -0,0 +1,32 @@
+#the dragon eye
+from zairfunctions import*
+
+
+bob.speed(0)
+
+bob.begin_fill()
+bob.circle(200)
+bob.color("red")
+bob.end_fill()
+jump(50,50)
+bob.circle(30)
+
+jump(-110,145)
+bob.begin_fill()
+for times in range(100):
+    bob.forward(220)
+    bob.left(128)
+bob.color("blue")
+bob.end_fill()
+
+bob.color("lime")
+jump(47,217)
+bob.begin_fill()
+for times in range(190):
+    bob.forward(100)
+    bob.left(178)
+bob.end_fill()
+
+
+
+
